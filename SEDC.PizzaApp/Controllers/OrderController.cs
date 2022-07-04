75524df9ac2362c489fc53b76d5209088ec39fe9
KristijanAdziovski <kristijanadziovskiadzi@gmail.com>
2022-07-04 23:29:33 +0200

    Add project files.

---
+++ SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.Mappers;
+using SEDC.PizzaApp.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Order> ordersDb = StaticDb.Orders;
+
+        
+            List<OrderDetailsViewModel> orderViewModels = ordersDb.Select(x => OrderMapper.ToOrderDetailsViewModel(x))
+                .ToList();
+
+
+            
+
+            ViewData["Title"] = "These are the orders made with our app:";
+            ViewData["NumberOfOrders"] = StaticDb.Orders.Count;
+
+            ViewData["FirstUser"] = StaticDb.Orders.First().User;
+
+            return View(orderViewModels);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new EmptyResult();
+            }
+
+            Order orderDb = StaticDb.Orders.FirstOrDefault(x => x.Id == id);
+            if(orderDb == null)
+            {
+              
+                return View("ResourceNotFound"); 
+            }
+
+            OrderDetailsViewModel orderDetailsViewModel = OrderMapper.ToOrderDetailsViewModel(orderDb);
+
+            ViewBag.Title = $"Details for order with id {id}";
+            ViewBag.User = orderDb.User;
+
+        
+            return View(orderDetailsViewModel);
+        }
+    }
+}
---
+++ SEDC.PizzaApp/Models/Mappers/OrderMapper.cs
@@ -0,0 +1,21 @@
+﻿using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Models.Mappers
+{
+    public static class OrderMapper
+    {
+       
+        public static OrderDetailsViewModel ToOrderDetailsViewModel(Order orderDb)
+        {
+            return new OrderDetailsViewModel
+            {
+                PaymentMethod = orderDb.PaymentMethod,
+                PizzaName = orderDb.Pizza.Name,
+                Price = orderDb.Pizza.Price + 100,
+                UserFullname = $"{orderDb.User.FirstName} {orderDb.User.LastName}",
+                Delivered = orderDb.Delivered
+            };
+        }
+    }
+}
